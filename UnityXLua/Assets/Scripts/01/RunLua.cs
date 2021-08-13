95d91b2164556e96d2eea38161ad707615180e15
douch <douch@163.com>
2021-08-13 15:27:59 +0800

    导入xlua 运行lua代码

---
+++ UnityXLua/Assets/Scripts/01/RunLua.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class RunLua : MonoBehaviour {
+
+    private LuaEnv luaenv;
+	// Use this for initialization
+	void Start () {
+        luaenv = new LuaEnv();
+        luaenv.DoString("print('Hello Xlua')");
+	}
+
+    private void OnDestroy()
+    {
+        luaenv.Dispose();
+    }
+}
