e937acd0d311e7313e0f5021bc0f2efdbbc512a8
douch <douch@163.com>
2021-08-13 15:49:51 +0800

    加载lua文件 执行lua文件中内容

---
+++ UnityXLua/Assets/Scripts/03/CallLuaFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class CallLuaFile : MonoBehaviour {
+
+    private LuaEnv luaeav;
+	// Use this for initialization
+	void Start () {
+        TextAsset luaFile = Resources.Load<TextAsset>("HelloXLua.lua");
+        luaeav = new LuaEnv();
+        luaeav.DoString(luaFile.text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
