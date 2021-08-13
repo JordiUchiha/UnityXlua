9246075485459f713a4099c98db42d5f6bdf37b7
douch <douch@163.com>
2021-08-13 15:35:53 +0800

    lua call cs debug

---
+++ UnityXLua/Assets/Scripts/02/LuaCallCsTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class LuaCallCsTest : MonoBehaviour {
+
+    private LuaEnv luaeav;
+	// Use this for initialization
+	void Start () {
+        luaeav = new LuaEnv();
+        luaeav.DoString("CS.UnityEngine.Debug.Log('Hello Xlua')");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
