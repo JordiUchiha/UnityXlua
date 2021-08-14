99dca733313604de019d10a4703b1d9548c853cf
douch <douch@163.com>
2021-08-14 14:30:00 +0800

    自定义loader加载lua

---
+++ UnityXLua/Assets/Scripts/04/CreateLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System.IO;
+public class CreateLoader : MonoBehaviour {
+
+    private byte[] MyLoader(ref string filePath)
+    {
+        string absPath = Application.streamingAssetsPath + "/" + filePath + ".lua.txt";
+        return System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(absPath));
+    }
+	// Use this for initialization
+	void Start () {
+        LuaEnv luaenv = new LuaEnv();
+
+        luaenv.AddLoader(MyLoader);
+
+        luaenv.DoString("require 'TestDefineLoader'");
+
+        luaenv.Dispose();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
