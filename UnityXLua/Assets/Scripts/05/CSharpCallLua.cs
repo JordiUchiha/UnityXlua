a2dd7897944c44eb78977f0077c0328d0f21cab9
douch <douch@163.com>
2021-08-14 14:42:08 +0800

    C# 调用Lua中的全局变量

---
+++ UnityXLua/Assets/Scripts/05/CSharpCallLua.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class CSharpCallLua : MonoBehaviour {
+
+    private LuaEnv luaeav;
+    // Use this for initialization
+    void Start()
+    {
+        luaeav = new LuaEnv();
+        //TextAsset luaFile = Resources.Load<TextAsset>("HelloXLua.lua");
+        //luaeav.DoString(luaFile.text);
+
+        luaeav.DoString("require 'CSharpCallLua'");
+
+        int a = luaeav.Global.Get<int>("a");
+        print(a);
+
+        string name = luaeav.Global.Get<string>("name");
+        print(name);
+
+        bool isWin = luaeav.Global.Get<bool>("isWin");
+        print(isWin);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
